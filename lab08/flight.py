309099ca4f4a7816b71f6ca0d68ae73fd0284088
Timothy Bretl <tbretl@illinois.edu>
2022-10-02 21:27:03 -0500

    fix lab08/flight.py

--- lab08/flight.py
+++ lab08/flight.py
@@ -12,7 +12,7 @@ uri = 'radio://0/0/2M/E7E7E7E7E7'
 
 # Specify the variables we want to log (all at 100 Hz)
 variables = [
-    # State estimates (stock code)
+    # State estimates (from default EKF)
     'ae483log.o_x',
     'ae483log.o_y',
     'ae483log.o_z',
@@ -49,17 +49,38 @@ class SimpleClient:
         self.use_observer = use_observer
         self.cf = Crazyflie(rw_cache='./cache')
         self.cf.connected.add_callback(self.connected)
+        self.cf.fully_connected.add_callback(self.fully_connected)
         self.cf.connection_failed.add_callback(self.connection_failed)
         self.cf.connection_lost.add_callback(self.connection_lost)
         self.cf.disconnected.add_callback(self.disconnected)
         print(f'Connecting to {uri}')
         self.cf.open_link(uri)
-        self.is_connected = False
+        self.is_fully_connected = False
         self.data = {}
 
     def connected(self, uri):
         print(f'Connected to {uri}')
-        self.is_connected = True
+    
+    def fully_connected(self, uri):
+        print(f'Fully connected to {uri}')
+        self.is_fully_connected = True
+
+        # Reset the default EKF
+        self.cf.param.set_value('kalman.resetEstimation', 1)
+
+        # Enable the controller (1 for default controller, 4 for ae483 controller)
+        if self.use_controller:
+            self.cf.param.set_value('stabilizer.controller', 4)
+            self.cf.param.set_value('powerDist.motorSetEnable', 1)
+        else:
+            self.cf.param.set_value('stabilizer.controller', 1)
+            self.cf.param.set_value('powerDist.motorSetEnable', 0)
+
+        # Enable the observer (0 for disable, 1 for enable)
+        if self.use_observer:
+            self.cf.param.set_value('ae483par.use_observer', 1)
+        else:
+            self.cf.param.set_value('ae483par.use_observer', 0)
 
         # Start logging
         self.logconfs = []
@@ -87,21 +108,6 @@ class SimpleClient:
                 for v in logconf.variables:
                     print(f' - {v.name}')
 
-        # Reset the stock EKF
-        self.cf.param.set_value('kalman.resetEstimation', 1)
-
-        # Enable the controller (1 for stock controller, 4 for ae483 controller)
-        if self.use_controller:
-            self.cf.param.set_value('stabilizer.controller', 4)
-        else:
-            self.cf.param.set_value('stabilizer.controller', 1)
-
-        # Enable the observer (0 for disable, 1 for enable)
-        if self.use_observer:
-            self.cf.param.set_value('ae483par.use_observer', 1)
-        else:
-            self.cf.param.set_value('ae483par.use_observer', 0)
-
     def connection_failed(self, uri, msg):
         print(f'Connection to {uri} failed: {msg}')
 
@@ -110,7 +116,7 @@ class SimpleClient:
 
     def disconnected(self, uri):
         print(f'Disconnected from {uri}')
-        self.is_connected = False
+        self.is_fully_connected = False
 
     def log_data(self, timestamp, data, logconf):
         for v in logconf.variables:
@@ -126,21 +132,9 @@ class SimpleClient:
         while time.time() - start_time < dt:
             self.cf.commander.send_position_setpoint(x, y, z, yaw)
             time.sleep(0.1)
-
-    def move_smooth(self, p1, p2, yaw, dt):
-        print(f'Move smoothly from {p1} to {p2} with yaw {yaw} degrees in {dt} seconds')
-        p1 = np.array(p1)
-        p2 = np.array(p2)
-        start_time = time.time()
-        while True:
-            current_time = time.time()
-            s = (current_time - start_time) / dt
-            p = (1 - s) * p1 + (s * p2)
-            self.cf.commander.send_position_setpoint(p[0], p[1], p[2], yaw)
-            if s >= 1:
-                return
-            else:
-                time.sleep(0.1)
+    
+    def move_smooth(self, p1, p2, yaw, speed):
+        pass # <-- FIXME (replace this line with your implementation of move_smooth)
 
     def stop(self, dt):
         print(f'Stop for {dt} seconds')
@@ -163,31 +157,31 @@ if __name__ == '__main__':
 
     # Create and start the client that will connect to the drone
     client = SimpleClient(uri, use_controller=True, use_observer=False)
-    while not client.is_connected:
-        print(f' ... connecting ...')
-        time.sleep(1.0)
+    while not client.is_fully_connected:
+        time.sleep(0.1)
 
     # Leave time at the start to initialize
     client.stop(1.0)
 
     # Take off and hover (with zero yaw)
     client.move(0.0, 0.0, 0.15, 0.0, 1.0)
-    client.move(0.0, 0.0, 0.50, 0.0, 1.0)
+    client.move_smooth([0.0, 0.0, 0.15], [0.0, 0.0, 0.5], 0.0, 0.2)
+    client.move(0.0, 0.0, 0.5, 0.0, 1.0)
 
     # Fly in a square five times (with a pause at each corner)
     num_squares = 5
     for i in range(num_squares):
-        client.move_smooth([0.0, 0.0, 0.5], [0.5, 0.0, 0.5], 0.0, 2.0)
+        client.move_smooth([0.0, 0.0, 0.5], [0.5, 0.0, 0.5], 0.0, 0.2)
         client.move(0.5, 0.0, 0.5, 0.0, 1.0)
-        client.move_smooth([0.5, 0.0, 0.5], [0.5, 0.5, 0.5], 0.0, 2.0)
+        client.move_smooth([0.5, 0.0, 0.5], [0.5, 0.5, 0.5], 0.0, 0.2)
         client.move(0.5, 0.5, 0.5, 0.0, 1.0)
-        client.move_smooth([0.5, 0.5, 0.5], [0.0, 0.5, 0.5], 0.0, 2.0)
+        client.move_smooth([0.5, 0.5, 0.5], [0.0, 0.5, 0.5], 0.0, 0.2)
         client.move(0.0, 0.5, 0.5, 0.0, 1.0)
-        client.move_smooth([0.0, 0.5, 0.5], [0.0, 0.0, 0.5], 0.0, 2.0)
+        client.move_smooth([0.0, 0.5, 0.5], [0.0, 0.0, 0.5], 0.0, 0.2)
         client.move(0.0, 0.0, 0.5, 0.0, 1.0)
-
+    
     # Go back to hover (with zero yaw) and prepare to land
-    client.move(0.0, 0.0, 0.50, 0.0, 1.0)
+    client.move_smooth([0.0, 0.0, 0.5], [0.0, 0.0, 0.15], 0.0, 0.2)
     client.move(0.0, 0.0, 0.15, 0.0, 1.0)
 
     # Land
