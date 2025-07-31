84e985f27cef8485b01f889fb1e4ed558f42cbb9
Timothy Bretl <tbretl@illinois.edu>
2025-07-31 18:47:09 -0500

    make capitalization of comments in ae483tools consistent

--- ae483tools.py
+++ ae483tools.py
@@ -31,7 +31,7 @@ def check_mocap_coverage(filename):
     ax.grid()    
 
 def resample_data_drone(raw_data, hz=1e2, t_min_offset=0., t_max_offset=0.):
-    # copy data (FIXME: may be unnecessary?)
+    # Copy data (FIXME: may be unnecessary?)
     data = {}
     for key, val in raw_data.items():
         data[key] = {
@@ -39,12 +39,12 @@ def resample_data_drone(raw_data, hz=1e2, t_min_offset=0., t_max_offset=0.):
             'data': val['data'].copy(),
         }
 
-    # convert lists to numpy arrays
+    # Convert lists to numpy arrays
     for key, val in data.items():
         val['data'] = np.array(val['data'], dtype=np.float64).copy()
         val['time'] = np.array(val['time'], dtype=np.float64).copy()
 
-    # find time interval
+    # Find time interval
     t_min = -np.inf
     t_max = np.inf
     for key, val in data.items():
@@ -57,47 +57,47 @@ def resample_data_drone(raw_data, hz=1e2, t_min_offset=0., t_max_offset=0.):
     t_min += t_min_offset
     t_max -= t_max_offset
 
-    # find zero time
+    # Find zero time
     t_zero = t_min
 
-    # do time shift
+    # Do time shift
     t_min -= t_zero
     t_max -= t_zero
     for key, val in data.items():
         val['time'] -= t_zero
 
-    # create an array of times at which to subsample
+    # Create an array of times at which to subsample
     if np.round(hz) != hz:
         raise Exception(f'hz = {hz} must be an integer')
     nt = int(1 + np.floor((t_max - t_min) * hz))
     t = t_min + np.arange(0, nt) / hz
     
-    # resample raw data with linear interpolation
+    # Resample raw data with linear interpolation
     resampled_data = {'time': t}
     for key, val in data.items():
         resampled_data[key] = np.interp(t, val['time'], val['data'])
         
-    # return the resampled data
+    # Return the resampled data
     return resampled_data
 
 
 def resample_data_mocap(raw_data, t, t_shift=0., t_min_offset=0.):
-    # copy data (FIXME: may be unnecessary?) and convert lists to numpy arrays
+    # Copy data (FIXME: may be unnecessary?) and convert lists to numpy arrays
     data = {}
     for key, val in raw_data.items():
         data[key] = np.array(val, dtype=np.float64).copy()
     
-    # find nan
+    # Find nan
     is_valid = np.ones_like(data['time']).astype('bool')
     for val in data.values():
         is_valid = np.bitwise_and(is_valid, ~np.isnan(val))
     i_is_valid = np.argwhere(is_valid).flatten()
 
-    # remove nan
+    # Remove nan
     for key, val in data.items():
         data[key] = val[i_is_valid]
     
-    # do time offset
+    # Do time offset
     t_min = data['time'][0]
     t_max = data['time'][-1]
     if (t_min_offset < 0.) or (t_min_offset > t_max - t_min):
@@ -109,16 +109,16 @@ def resample_data_mocap(raw_data, t, t_shift=0., t_min_offset=0.):
     for key, val in data.items():
         data[key] = val[i_min:]
     
-    # do time shift
+    # Do time shift
     data['time'] -= (data['time'][0] - t_shift)
 
-    # resample raw data with linear interpolation
+    # Resample raw data with linear interpolation
     resampled_data = {'time': t.copy()}
     for key, val in data.items():
         if key != 'time':
             resampled_data[key] = np.interp(t, data['time'], val)
     
-    # return the resampled data
+    # Return the resampled data
     return resampled_data
 
 
